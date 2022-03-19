5e5abf40f11a6a84ff00de7f5ad3dba2551d3561
Massii <massinechiche@gmail.com>
2022-03-19 15:55:00 +0100

    chore: add bid button

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -48,6 +48,24 @@ const NFTCard = ({ data }) => {
                     titleSize={SIZES.large}
                     subTitleSize={SIZES.small}
                 />
+
+                <View
+                    style={{
+                        marginTop: SIZES.font,
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
+                >
+                    <EthPrice price={data.price} />
+                    <RectButton
+                        minWidth={120}
+                        fontSize={SIZES.font}
+                        handlePress={() =>
+                            navigation.navigate('Details', { data })
+                        }
+                    />
+                </View>
             </View>
         </View>
     )
