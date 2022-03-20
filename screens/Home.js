d43b92dca4343e170691f2f8324b0d6ef3721810
Massii <massinechiche@gmail.com>
2022-03-20 19:55:00 +0100

    chore: change homescreen

--- screens/Home.js
+++ screens/Home.js
@@ -29,9 +29,11 @@ const Home = () => {
                     }}
                 >
                     <View
-                        style={{ height: 300, backgroundColor: COLORS.primary }}
+                        style={{
+                            height: '100%',
+                            backgroundColor: COLORS.primary,
+                        }}
                     />
-                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
                 </View>
             </View>
         </SafeAreaView>
