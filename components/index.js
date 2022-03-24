5460adfc3a06aa413f957ddbad9892c854b25d81
Massii <massinechiche@gmail.com>
2022-03-24 20:50:00 +0100

    fix: subInfo import

--- components/index.js
+++ components/index.js
@@ -2,5 +2,17 @@ import FocusedStatusBar from './FocusedStatusBar'
 import HomeHeader from './HomeHeader'
 import NFTCard from './NFTCard'
 import { CircleButton, RectButton } from './Button'
+import DetailsDesc from './DetailsDesc'
+import DetailsBid from './DetailsBid'
+import { SubInfo } from './SubInfo'
 
-export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton }
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    CircleButton,
+    RectButton,
+    DetailsBid,
+    DetailsDesc,
+    SubInfo,
+}
